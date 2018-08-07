bbe6a110204d2353f275377fd0fbc6f071951f44
Joseph M Spinelli <josephspinelli@yahoo.com>
2018-08-07 16:15:52 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
-def display_rainbow(colors)
-  colors = (["red", "orange", "yellow", "green", "blue", "indigo", "violet",])
+def display_rainbow
+  display_rainbow (["red", "orange", "yellow", "green", "blue", "indigo", "violet",])
   puts "R: #{colors[0]}"
   puts "O: #{colors[1]}"
   puts "Y: #{colors[2]}"
